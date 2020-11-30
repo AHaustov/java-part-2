14ee39bfa7d3ec9b048100220f36505d748a9a82
AHaustov <anton.haust@gmail.com>
2020-11-30 13:19:04 +0100

    HW 06 anton

--- src/day06/excercise1/BankApp.java
+++ src/day06/excercise1/BankApp.java
@@ -11,7 +11,7 @@ public class BankApp {
 
         LoanConsumer maria = new LoanConsumer("Maria", 17, 5000);
         LoanConsumer ted = new LoanConsumer("Theodor", 31, 25000);
-        LoanConsumer agatha = new LoanConsumer("Agatha", 55, 5000);
+        LoanConsumer agatha = new LoanConsumer("Agatha", 55, 25000);
         LoanConsumer arthur = new LoanConsumer("Arthur", 71, 50000);
         LoanConsumer kevin = new LoanConsumer("Kevin", 21, 15000);
         LoanConsumer chantale = new LoanConsumer("Chantale", 29, 31000);
@@ -19,15 +19,28 @@ public class BankApp {
 
         LoanConsumer[] consumer = new LoanConsumer[]{maria, ted, agatha, arthur, kevin, chantale, brat};
 
-        printIssue(employees, consumer);
+        printIssueEmployee(employees, consumer);
+        printIssueConsumer(employees, kevin, agatha, arthur);
     }
 
-    private static void printIssue(LoanIssuer[] employees, LoanConsumer[] consumer) {
+    private static void printIssueConsumer(LoanIssuer[] employees, LoanConsumer... consumer) {
+        for (LoanConsumer consume : consumer) {
+            for (LoanIssuer issuer : employees) {
+                if (issuer.toIssue(consume)) {
+                    System.out.println(consume.getName() + " is getting a loan from Bank employee " + issuer.getName());
+                } else
+                    System.out.println(consume.getName() + " is not getting a loan from Bank employee " + issuer.getName());
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printIssueEmployee(LoanIssuer[] employees, LoanConsumer[] consumer) {
         for (LoanIssuer issuer : employees) {
             for (LoanConsumer consume : consumer) {
                 if (issuer.toIssue(consume)) {
                     System.out.println("Bank employee " + issuer.getName() + " issues a loan to " + consume.getName());
-                }else
+                } else
                     System.out.println("Bank employee " + issuer.getName() + " doesn't issues a loan to " + consume.getName());
             }
             System.out.println();
---
+++ src/day06/excercise1/BankAppTest.java
@@ -0,0 +1,64 @@
+package day06.excercise1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAppTest {
+    LoanIssuer tom = new LoanIssuer("Thomas", true, true);
+    LoanIssuer bob = new LoanIssuer("Robert", false, true);
+    LoanIssuer luc = new LoanIssuer("Lucas", true, false);
+    LoanIssuer carl = new LoanIssuer("Carl", false, false);
+
+
+    LoanConsumer maria = new LoanConsumer("Maria", 17, 5000);
+    LoanConsumer ted = new LoanConsumer("Theodor", 31, 25000);
+    LoanConsumer agatha = new LoanConsumer("Agatha", 55, 35000);
+    LoanConsumer arthur = new LoanConsumer("Arthur", 71, 50000);
+    LoanConsumer kevin = new LoanConsumer("Kevin", 21, 15000);
+    LoanConsumer chantale = new LoanConsumer("Chantale", 29, 31000);
+
+    @Test
+    void testAge_under18_false() {
+        assertFalse(tom.toIssue(maria));
+        assertFalse(bob.toIssue(maria));
+        assertFalse(luc.toIssue(maria));
+        assertFalse(carl.toIssue(maria));
+    }
+    @Test
+    void testIncome_customerUnder20k_false(){
+        assertFalse(tom.toIssue(kevin));
+        assertFalse(bob.toIssue(kevin));
+        assertFalse(luc.toIssue(kevin));
+        assertFalse(carl.toIssue(kevin));
+    }
+
+    @Test
+    void testIsLazy_customerOlder18_ok() {
+        assertTrue(tom.toIssue(ted));
+        assertTrue(tom.toIssue(agatha));
+        assertTrue(tom.toIssue(arthur));
+        assertTrue(tom.toIssue(chantale));
+
+        assertTrue(luc.toIssue(ted));
+        assertTrue(luc.toIssue(agatha));
+        assertTrue(luc.toIssue(arthur));
+        assertTrue(luc.toIssue(chantale));
+    }
+
+    @Test
+    void testIsKindNotLazy_customerUnder70_ok() {
+        assertFalse(bob.toIssue(maria));
+        assertTrue(bob.toIssue(ted));
+        assertTrue(bob.toIssue(agatha));
+        assertFalse(bob.toIssue(arthur));
+        assertFalse(bob.toIssue(kevin));
+        assertTrue(bob.toIssue(chantale));
+    }
+
+    @Test
+    void testNotLazyNotKind_customerOver70AndOver20k_false() {
+        assertFalse(carl.toIssue(arthur));
+    }
+
+}
