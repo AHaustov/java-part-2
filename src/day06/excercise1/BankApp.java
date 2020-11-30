2b48bbf53b517430bcce542d7661c22075a8223f
AHaustov <anton.haust@gmail.com>
2020-11-30 12:43:57 +0100

    bank

---
+++ src/day06/excercise1/BankApp.java
@@ -0,0 +1,36 @@
+package day06.excercise1;
+
+// Bank employee name issues a loan to name
+public class BankApp {
+    public static void main(String[] args) {
+        LoanIssuer tom = new LoanIssuer("Thomas", true, true);
+        LoanIssuer bob = new LoanIssuer("Robert", false, true);
+        LoanIssuer luc = new LoanIssuer("Lucas", true, false);
+        LoanIssuer carl = new LoanIssuer("Carl", false, false);
+        LoanIssuer[] employees = new LoanIssuer[]{tom, bob, luc, carl};
+
+        LoanConsumer maria = new LoanConsumer("Maria", 17, 5000);
+        LoanConsumer ted = new LoanConsumer("Theodor", 31, 25000);
+        LoanConsumer agatha = new LoanConsumer("Agatha", 55, 5000);
+        LoanConsumer arthur = new LoanConsumer("Arthur", 71, 50000);
+        LoanConsumer kevin = new LoanConsumer("Kevin", 21, 15000);
+        LoanConsumer chantale = new LoanConsumer("Chantale", 29, 31000);
+        LoanConsumer brat = new LoanConsumer("Brat", 31, 31000);
+
+        LoanConsumer[] consumer = new LoanConsumer[]{maria, ted, agatha, arthur, kevin, chantale, brat};
+
+        printIssue(employees, consumer);
+    }
+
+    private static void printIssue(LoanIssuer[] employees, LoanConsumer[] consumer) {
+        for (LoanIssuer issuer : employees) {
+            for (LoanConsumer consume : consumer) {
+                if (issuer.toIssue(consume)) {
+                    System.out.println("Bank employee " + issuer.getName() + " issues a loan to " + consume.getName());
+                }else
+                    System.out.println("Bank employee " + issuer.getName() + " doesn't issues a loan to " + consume.getName());
+            }
+            System.out.println();
+        }
+    }
+}
