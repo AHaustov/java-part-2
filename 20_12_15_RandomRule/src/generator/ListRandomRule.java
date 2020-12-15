7ecfc6e567e051b118f6c18443528275176d3fdd
AHaustov <anton.haust@gmail.com>
2020-12-15 19:35:01 +0100

    ListRandom updated

--- 20_12_15_RandomRule/src/generator/ListRandomRule.java
+++ 20_12_15_RandomRule/src/generator/ListRandomRule.java
@@ -1,14 +1,15 @@
 package generator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class ListRandomRule implements RandomRule {
-    private List<Integer> values;
+    private ArrayList<Integer> values;
     private Random random = new Random();
 
     public ListRandomRule(List<Integer> values) {
-        this.values = values;
+        this.values = new ArrayList<>(values);
     }
 
     public ListRandomRule(int... values) {
