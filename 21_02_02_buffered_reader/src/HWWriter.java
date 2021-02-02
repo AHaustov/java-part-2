151127fe95d432c2e3e3661a615994b2de6e3a93
AHaustov <anton.haust@gmail.com>
2021-02-02 17:48:35 +0100

    new version

--- 21_02_02_buffered_reader/src/HWWriter.java
+++ 21_02_02_buffered_reader/src/HWWriter.java
@@ -8,12 +8,13 @@ public class HWWriter {
     public static void writeFromConsole() throws IOException {
         FileOutputStream fos = new FileOutputStream("output.txt");
         PrintWriter pw = new PrintWriter(fos);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String line;
 
-        while ((line = br.readLine()) != null && !line.equals("exit")) {
-            pw.println(line);
+            while ((line = br.readLine()) != null && !line.equals("exit")) {
+                pw.println(line);
+            }
+            pw.flush();
         }
-        pw.flush();
     }
 }
