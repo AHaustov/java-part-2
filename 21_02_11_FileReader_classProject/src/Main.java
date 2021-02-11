29b930031edc37868b08b5b7fff9031efd172187
AHaustov <anton.haust@gmail.com>
2021-02-12 00:46:21 +0100

    hw

--- 21_02_11_FileReader_classProject/src/Main.java
+++ 21_02_11_FileReader_classProject/src/Main.java
@@ -7,28 +7,24 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException, InterruptedException {
 
         BlockingQueue<String> queue = new LinkedBlockingQueue<>();
         OperationContext oc = new OperationContext();
-        try (PrintWriter pw = new PrintWriter(new FileWriter("output.txt"));
-             BufferedReader br = new BufferedReader(new FileReader("input.txt"))
-        ) {
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            try (PrintWriter pw = new PrintWriter(new FileWriter("output.txt"))) {
+                Thread supplier = new Thread(new Supplier(br, queue));
+                Thread consumer = new Thread(new Consumer(queue, pw, oc));
+                supplier.isDaemon();
+                consumer.isDaemon();
 
-            Thread supplier = new Thread(new Supplier(br, queue));
-            Thread consumer = new Thread(new Consumer(queue, pw, oc));
-            supplier.isDaemon();
-            consumer.isDaemon();
+                supplier.start();
+                consumer.start();
 
-            supplier.start();
-            consumer.start();
+                supplier.join();
+                consumer.join();
 
-            supplier.join();
-            consumer.join();
-
+            }
         }
     }
-
-
 }
