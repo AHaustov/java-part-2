556a7bacf3c2182fe40a6d8a924142b3902c00b6
AHaustov <anton.haust@gmail.com>
2021-02-22 10:28:50 +0100

    intersect

--- 21_02_19_lambdaHW2/src/Intersect.java
+++ 21_02_19_lambdaHW2/src/Intersect.java
@@ -4,7 +4,7 @@ import java.util.function.IntPredicate;
 public class Intersect {
 
     IntPredicate intersect(List<IntPredicate> predicates) {
-        IntPredicate res = null;
+        IntPredicate res = x -> true;
         for (int i = 0; i < predicates.size(); i++) {
             res = predicates.get(i).and(res);
         }
